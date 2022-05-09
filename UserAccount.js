6184a1afe2f03352e8f5f494472538079d96fd85
dave <dave41264@hotmail.com>
2022-05-09 14:46:12 +0100

    Combined UserAccountUI and UserAccount namespaces

--- UserAccount.js
+++ UserAccount.js
@@ -22,6 +22,209 @@ namespace UserAccount
 	reg authToken = loadToken();
 	reg online = Server.isOnline();;
     
+    // pnlLogin
+    const pnlLogin = Content.getComponent("pnlLogin");
+    
+    pnlLogin.setPaintRoutine(function(g)
+    {
+    	var a = this.getLocalBounds(0);
+    
+    	g.setColour(this.get("bgColour"));
+    	g.fillRoundedRectangle(a, 5);
+    
+    	g.setColour(this.get("itemColour"));
+    	g.fillRoundedRectangle([a[0], a[1], a[2], 150], 3);
+    	
+    	g.setFont("medium", 26);
+    	g.setColour(this.get("textColour"));
+    	g.drawAlignedText("Login", [a[0] + 15, 0, a[2], 150], "centred");
+    	
+    	g.setColour(this.get("itemColour2"));
+    	g.fillPath(Paths.icons["user"], [a[0] + a[2] / 2 - 50, 150 / 2 - 15, 21, 30]);
+    	
+    	LookAndFeel.drawInput(lblUsername, {id: "email", width: 18, height: 14});
+    	LookAndFeel.drawInput(lblPassword, {id: "lock", width: 18, height: 20});
+    });
+    
+    // lblUsername
+    const lblUsername = Content.getComponent("lblUsername");
+    	
+    // lblPassword 
+    const lblPassword = Content.getComponent("lblPassword");
+    lblPassword.set("fontStyle", "Password");
+    
+    // btnShowPassword
+    const btnShowPassword = Content.getComponent("btnShowPassword");
+    btnShowPassword.setLocalLookAndFeel(LookAndFeel.iconButton);
+    btnShowPassword.setControlCallback(onbtnShowPasswordControl);
+        
+    inline function onbtnShowPasswordControl(component, value)
+    {
+    	if (value)
+    		lblPassword.set("fontStyle", "plain");
+    	else
+    		lblPassword.set("fontStyle", "Password");			
+    }
+    
+    // btnLogin
+	const btnLogin = Content.getComponent("btnLogin");
+	btnLogin.setLocalLookAndFeel(LookAndFeel.textButton);
+	btnLogin.setControlCallback(onbtnLoginControl);
+
+	inline function onbtnLoginControl(component, value)
+	{
+		if (!value)
+			login(lblUsername.get("text"), lblPassword.get("text"));
+	}
+        
+	// btnLogout
+	const btnLogout = Content.getComponent("btnLogout");
+	btnLogout.set("enabled", true);
+	btnLogout.setLocalLookAndFeel(LookAndFeel.iconButton);
+	btnLogout.setControlCallback(onbtnLogoutControl);
+        
+	inline function onbtnLogoutControl(component, value)
+	{
+		if (!value)
+		{
+			if (UserAccount.isLoggedIn() == true)
+			{
+				Engine.showYesNoWindow("Confirm Logout", "Do you want to logout?", function(response)
+				{
+					if (response)
+						logout();
+				});
+			}
+			else
+			{
+				logout();
+			}
+		}    		
+	}
+            
+    // btnRegister
+    const btnRegister = Content.getComponent("btnRegister");
+    btnRegister.setLocalLookAndFeel(LookAndFeel.textButton);
+    btnRegister.setControlCallback(onbtnRegisterControl);
+    
+    inline function onbtnRegisterControl(component, value)
+    {
+    	if (!value)
+    	{
+    		btnRegisterAcceptPolicy.setValue(false);
+    		pnlRegister.showControl(true);
+    	}			
+    }
+    
+    // btnLoginRecovery
+    const btnLoginRecovery = Content.getComponent("btnLoginRecovery");
+    btnLoginRecovery.setLocalLookAndFeel(LookAndFeel.textButton);
+    btnLoginRecovery.setControlCallback(onbtnLoginRecoveryControl);
+    
+    inline function onbtnLoginRecoveryControl(component, value)
+    {
+    	if (!value)
+    		Engine.openWebsite(Config.baseURL[Config.MODE] + "/my-account/lost-password/");
+    }
+    
+    // pnlRegister
+    const pnlRegister = Content.getComponent("pnlRegister");
+    
+    pnlRegister.setPaintRoutine(function(g)
+    {
+    	var a = this.getLocalBounds(0);
+    
+    	g.setColour(this.get("bgColour"));
+    	g.fillRoundedRectangle(a, 5);
+    	
+    	g.setFont("medium", 26);
+    	g.setColour(this.get("textColour"));
+    	g.drawAlignedText("Create New Account", [lblRegisterEmail.get("x") + 25, lblRegisterEmail.get("y") - 60, lblRegisterEmail.getWidth(), 30], "left");
+    	
+    	g.fillPath(Paths.icons["newUser"], [lblRegisterEmail.get("x") - 30, lblRegisterEmail.get("y") - 60, 37, 32]);
+    	
+    	g.setFont("medium", 18);
+    	g.drawAlignedText("Accept", [lblRegisterEmail.get("x") - 10, lblRegisterEmail.get("y") + 40, 50, 30], "left");
+    	
+    	g.setColour(this.get("itemColour2"));
+    	g.drawAlignedText("Privacy Policy", [lblRegisterEmail.get("x") + 42, lblRegisterEmail.get("y") + 40, 100, 30], "left");
+    	g.drawHorizontalLine(btnRegisterViewPolicy.get("y") + btnRegisterViewPolicy.getHeight(), btnRegisterViewPolicy.get("x"), btnRegisterViewPolicy.get("x") + btnRegisterViewPolicy.getWidth());
+    
+    	LookAndFeel.drawInput(lblRegisterEmail, {id: "email", width: 18, height: 14});
+    });
+    
+    // lblRegisterEmail
+    const lblRegisterEmail = Content.getComponent("lblRegisterEmail");
+    lblRegisterEmail.set("text", "Email");	
+    
+    // btnRegisterAcceptPolicy
+    const btnRegisterAcceptPolicy = Content.getComponent("btnRegisterAcceptPolicy");
+    btnRegisterAcceptPolicy.setLocalLookAndFeel(LookAndFeel.checkBox);
+    btnRegisterAcceptPolicy.setControlCallback(onbtnRegisterAcceptPolicyControl);
+    
+    inline function onbtnRegisterAcceptPolicyControl(component, value)
+    {
+    	btnRegisterSubmit.set("enabled", value);
+    }
+    
+    // btnRegisterViewPolicy
+    const btnRegisterViewPolicy = Content.getComponent("btnRegisterViewPolicy");
+    btnRegisterViewPolicy.setLocalLookAndFeel(LookAndFeel.empty);
+    btnRegisterViewPolicy.setControlCallback(onbtnRegisterViewPolicyControl);
+    
+    inline function onbtnRegisterViewPolicyControl(component, value)
+    {
+    	if (!value)
+    		Engine.openWebsite(Config.baseURL[Config.MODE] + "/privacy-policy");
+    }
+    
+    // btnRegisterCancel
+    const btnRegisterCancel = Content.getComponent("btnRegisterCancel");
+    btnRegisterCancel.setLocalLookAndFeel(LookAndFeel.textButton);
+    btnRegisterCancel.setControlCallback(onbtnRegisterCancelControl);
+    
+    inline function onbtnRegisterCancelControl(component, value)
+    {
+    	if (!value)
+    		pnlRegister.showControl(false);
+    }
+    
+    // btnRegisterSubmit
+    const btnRegisterSubmit = Content.getComponent("btnRegisterSubmit");
+    btnRegisterSubmit.set("enabled", false);
+    btnRegisterSubmit.setLocalLookAndFeel(LookAndFeel.textButton);
+    btnRegisterSubmit.setControlCallback(onbtnRegisterSubmitControl);
+    
+    inline function onbtnRegisterSubmitControl(component, value)
+    {
+    	if (!value)
+    	{
+    		local email = lblRegisterEmail.get("text");
+    
+    		if (Server.isEmailAddress(email))
+    			createAccount(email);	
+    		else 
+    			Engine.showMessageBox("Email Address", "Please enter a valid email address.", 3);
+    	}
+    }		
+         	
+    // btnOfflineMode
+    const btnOfflineMode = Content.getComponent("btnOfflineMode");
+    btnOfflineMode.setLocalLookAndFeel(LookAndFeel.textButton);
+    btnOfflineMode.setControlCallback(onbtnOfflineModeControl);
+    
+    inline function onbtnOfflineModeControl(component, value)
+    {
+    	if (!value)
+    	{
+    		logout();
+    		LibraryHeader.setEnabledStateOfButtons();
+    		LibraryList.show();
+    		Library.updateCatalogue();
+            hide();
+    	}
+    }
+    
 	// Functions
 	inline function createAccount(email)
 	{		
@@ -32,7 +235,7 @@ namespace UserAccount
 		Server.setBaseURL(Config.baseURL[Config.MODE]);
 		
 		Spinner.show("Communicating with server");
-		
+
 		Server.callWithPOST(endpoint, p, function(status, response)
 		{
 			Spinner.hide();
@@ -59,7 +262,7 @@ namespace UserAccount
 	
 	inline function login(username, password)
     {
-        local p = {"username": username.trim(), "password": password};
+        var p = {"username": username.trim(), "password": password};
         
         if (!isDefined(username) || username == "" || username == "Email")
 			return Engine.showMessageBox("Invalid Email", "Please enter a valid email address or username.", 3);
@@ -78,11 +281,13 @@ namespace UserAccount
 
 			if (status == 200 && isDefined(response.token))
 		    {
-				storeToken(response.token);
+				storeCredentials(p.username, response.token);
 		        Library.clearCache();
 		        Library.rebuildCache();
+		        LibraryHeader.setEnabledStateOfButtons();
 		        Engine.rebuildCachedPools();
-		        UserAccountUI.hideLoginScreen();
+		        hide();
+		        LibraryList.show();
 		    }
 		    else
 		    {
@@ -107,7 +312,7 @@ namespace UserAccount
 		         
 	inline function logout()
 	{
-		UserAccountUI.showLoginScreen();
+		showLoginScreen();
 		deleteCredentials();
 		Library.clearCache();
 	}
@@ -121,24 +326,30 @@ namespace UserAccount
 			f.deleteFileOrDirectory();
 	}
         
-	inline function storeToken(token)
+	inline function storeCredentials(username, token)
      {
-		local data = {"token": token};
+		local data = {"username": username, "token": token};
 		local f = appData.getChildFile("credentials.json");
 		
 		authToken = token;
-		f.writeEncryptedObject(data, encryptionKey);
+		f.writeEncryptedObject(data, Config.encryptionKey);
      }
     
 	inline function loadToken()
 	{
 		local f = appData.getChildFile("credentials.json");
-		local data = f.loadEncryptedObject(encryptionKey);
 
-		if (f.isFile())
+		if (isDefined(f) && f.isFile())
+		{
+			local data = f.loadEncryptedObject(Config.encryptionKey);
+			
+			if (isDefined(Config.ENCODE_EXPANSIONS) && Config.ENCODE_EXPANSIONS)
+				Expansions.setCredentials(data.username);
+				
 			return data.token;
+		}
 
-		return false;
+		return undefined;
 	}
 
 	inline function getToken()
@@ -148,7 +359,7 @@ namespace UserAccount
 	
 	inline function isLoggedIn()
 	{
-		return isDefined(authToken) && authToken != 0;
+		return isDefined(authToken);
 	}
 	
 	inline function isOnline()
@@ -163,7 +374,7 @@ namespace UserAccount
 	
 	inline function isOnlineAndIsLoggedInSilent()
 	{
-		return isDefined(authToken) && authToken != 0 && online;
+		return isDefined(authToken) && online;
 	}
 	
 	inline function isOnlineAndIsLoggedIn()
@@ -173,7 +384,7 @@ namespace UserAccount
 		if (!online)
 			return Engine.showMessageBox("Offline Mode", "An internet connection is required.", 1);
 			
-		if (!isDefined(authToken) || authToken == 0)
+		if (!isDefined(authToken))
 			return Engine.showMessageBox("Login Required", "You need to login to use this feature.", 1);
 			
 		if (Engine.isPlugin() && !Config.NETWORK_IN_PLUGIN)
@@ -181,4 +392,29 @@ namespace UserAccount
 		
 		return true;
 	}
+	
+	inline function hide()
+	{
+		pnlRegister.showControl(false);
+		pnlLogin.showControl(false);
+	}
+	
+	inline function showLoginScreen()
+	{
+		lblUsername.set("text", "");
+		lblPassword.set("text", "");
+		pnlRegister.showControl(false);
+		pnlLogin.showControl(true);
+	}
+	
+	inline function hideLoginScreen()
+	{
+		pnlLogin.showControl(false);
+	}
+
+	// Function calls
+	if (!isDefined(UserAccount.getToken()))
+		showLoginScreen();
+	else
+		hide();
 }
