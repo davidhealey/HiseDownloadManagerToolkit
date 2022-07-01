d4cdc1db43fb3bd95c78fbaa9bf5824f6051fbaa
dave <dave41264@hotmail.com>
2022-07-02 00:38:40 +0100

    Redo user settings

--- UserSettings.js
+++ UserSettings.js
@@ -1,44 +1,37 @@
 namespace UserSettings
 {
-	reg data = {"vstPath": "", "auPath": ""};
+	const NUM_TABS = 2;
+	const appData = FileSystem.getFolder(FileSystem.AppData);
+	
+	reg data = {};
 
-	// btnSettings
-	const btnSettings = Content.getComponent("btnSettings");
-	btnSettings.setControlCallback(onbtnSettingsControl);
-	
-	inline function onbtnSettingsControl(component, value)
-	{
-		if (value)
-			pnlSettings.showControl(value);
-	}
-	
 	// pnlSettings
 	const pnlSettings = Content.getComponent("pnlSettings");
-	
+
 	pnlSettings.setPaintRoutine(function(g)
 	{
-		g.fillAll(Colours.withAlpha(Colours.black, 0.8));
+		g.fillAll(this.get("bgColour"));
 	});
+		
+	// btnSettingsClose
+	const btnSettingsClose = Content.getComponent("btnSettingsClose");
+	btnSettingsClose.setLocalLookAndFeel(LookAndFeel.textButton);
+	btnSettingsClose.setControlCallback(onbtnSettingsCloseControl);
 	
-	// pnlSettingsContainer
-	const pnlSettingsContainer = Content.getComponent("pnlSettingsContainer");
-	
-	pnlSettingsContainer.setPaintRoutine(function(g)
+	inline function onbtnSettingsCloseControl(component, value)
 	{
-		g.setColour(this.get("bgColour"));
-		g.fillRoundedRectangle([0, 0, this.getWidth(), this.getHeight()], 5);
-	});
+		if (!value)
+			saveAndExit();
+	}
 	
-	
-	// btnSettingsTab, pnlSettingsTab
+	// btnSettingsTab
 	const btnSettingsTab = [];
-	const pnlSettingsTab = [];
 	
-	for (i = 0; i < 2; i++)
+	for (i = 0; i < NUM_TABS; i++)
 	{
 		btnSettingsTab.push(Content.getComponent("btnSettingsTab" + i));
+		btnSettingsTab[i].setLocalLookAndFeel(LookAndFeel.textToggleButton);
 		btnSettingsTab[i].setControlCallback(onbtnSettingsTabControl);
-		pnlSettingsTab.push(Content.getComponent("pnlSettingsTab" + i));
 	}
 	
 	inline function onbtnSettingsTabControl(component, value)
@@ -46,326 +39,287 @@ namespace UserSettings
 		local index = btnSettingsTab.indexOf(component);
 		
 		for (i = 0; i < btnSettingsTab.length; i++)
-		{
 			btnSettingsTab[i].setValue(i == index);
-			pnlSettingsTab[i].showControl(i == index);
-		}
+		
+		changeTab(index);
 	}
 	
-	// pnlSettingsTab0
+	// pnlSettingsTab
+	const pnlSettingsTab = [];
+	
+	for (i = 0; i < NUM_TABS; i++)
+	{
+		pnlSettingsTab.push(Content.getComponent("pnlSettingsTab" + i));
+		pnlSettingsTab[i].showControl(btnSettingsTab[i].getValue());
+	}
+	
+	// pnlSettingsTab0 - Library
 	pnlSettingsTab[0].setPaintRoutine(function(g)
 	{
-		g.setColour(this.get("bgColour"));
-		g.fillRoundedRectangle([0, 0, this.getWidth(), this.getHeight()], 5);
+		var a = this.getLocalBounds(0);
 
-		LookAndFeel.drawInput(lblVstPath, "");
-
-		if (lblAuPath.get("visible"))
-			LookAndFeel.drawInput(lblAuPath, "");
+		var lblArea = [lblSamplePath.get("x") - 5, lblSamplePath.get("y"), lblSamplePath.getWidth() + 40, lblSamplePath.getHeight()];
 		
-		g.setFont("medium", 20);
-		g.setColour(this.get("textColour"));
-		g.drawAlignedText("VST3 Path", [lblVstPath.get("x") - 115, lblVstPath.get("y") + lblVstPath.getHeight() - lblVstPath.getHeight() / 2 - 14, 100, 30], "left");
+		g.setColour(lblSamplePath.get("bgColour"));
+		g.fillRoundedRectangle(lblArea, 3);
+		
+		lblArea = [lblVst3Path.get("x") - 5, lblVst3Path.get("y"), lblVst3Path.getWidth() + 40, lblVst3Path.getHeight()];
+		
+		g.setColour(lblVst3Path.get("bgColour"));
+		g.fillRoundedRectangle(lblArea, 3);
 		
 		if (lblAuPath.get("visible"))
-			g.drawAlignedText("AU Path", [lblAuPath.get("x") - 115, lblAuPath.get("y") + lblAuPath.getHeight() - lblAuPath.getHeight() / 2 - 14, 100, 30], "left");
+		{
+			lblArea = [lblAuPath.get("x") - 5, lblAuPath.get("y"), lblAuPath.getWidth() + 40, lblAuPath.getHeight()];
+			
+			g.setColour(lblAuPath.get("bgColour"));
+			g.fillRoundedRectangle(lblArea, 3);
+		}
 
-		g.drawAlignedText("Auto Sync", [lblVstPath.get("x") - 115, btnAutoSync.get("y") + btnAutoSync.getHeight() - btnAutoSync.getHeight() / 2 - 16, 100, 30], "left");
-		g.drawAlignedText("Manual Install", [lblVstPath.get("x") - 115, btnManualInstall.get("y") + btnManualInstall.getHeight() - btnManualInstall.getHeight() / 2 - 15, 150, 30], "left");
-		g.drawAlignedText("Temporary Files", [lblVstPath.get("x") - 115, btnClearTemp.get("y") + btnClearTemp.getHeight() - btnClearTemp.getHeight() / 2 - 15, 150, 30], "left");
-		g.drawAlignedText("Sample Quality", [lblVstPath.get("x") - 115, btnBitDepth.get("y") + btnBitDepth.getHeight() - btnBitDepth.getHeight() / 2 - 16, 150, 30], "left");
-
+		// Labels
+		g.setColour(lblSamplePath.get("textColour"));
 		g.setFont("medium", 18);
-		g.setColour(btnAutoSync.get("textColour"));
-		var t = btnAutoSync.getValue() ? "ON" : "OFF";
-		g.drawAlignedText(t, [btnAutoSync.get("x") + btnAutoSync.getWidth() + 10, btnAutoSync.get("y"), 75, btnAutoSync.getHeight()], "left");
-		
-		g.setColour(btnBitDepth.get("textColour"));
-		t = btnBitDepth.getValue() ? "24-BIT" : "16-BIT";
-		g.drawAlignedText(t, [btnBitDepth.get("x") + btnBitDepth.getWidth() + 10, btnBitDepth.get("y"), 75, btnBitDepth.getHeight()], "left");
-	});
-	
-	// pnlSettingsTab1
-	pnlSettingsTab[1].setPaintRoutine(function(g)
-	{
-		g.setColour(this.get("bgColour"));
-		g.fillRoundedRectangle([0, 0, this.getWidth(), this.getHeight()], 5);
-	});
-	
-	// fltEngineSettings
-	const fltEngineSettings = Content.getComponent("fltEngineSettings");
-	const tileData = {
-		"Type": "CustomSettings",
-		"Title": "Engine",
-		"ColourData": {
-			"bgColour": 0x00000000,
-			"textColour": 0xFF8C8780
-		},
-		"Font": "medium",
-		"FontSize": 16,
-		"Driver": !isPlugin,
-		"Device": !isPlugin,
-		"Output": !isPlugin,
-		"BufferSize": !isPlugin,
-		"SampleRate": !isPlugin,
-		"GlobalBPM": true,
-		"StreamingMode": true,
-		"GraphicRendering": true,
-		"ScaleFactor": true,
-		"SustainCC": false,
-		"ClearMidiCC": false,
-		"SampleLocation": false,
-		"DebugMode": false,
-		"ScaleFactorList": [0.5, 0.75, 1.0, 1.25, 1.5, 2.0]
-	};
-	
-	fltEngineSettings.setContentData(tileData);
-	
-	// btnVstPath
-	const btnVstPath = Content.getComponent("btnVstPath");
-	btnVstPath.setControlCallback(onbtnVstPathControl);
-	
-	inline function onbtnVstPathControl(component, value)
-	{
-		if (value)
-			setVstPath();
-	}
-	
-	// lblVstPath
-	const lblVstPath = Content.getComponent("lblVstPath");
-	lblVstPath.set("text", "");
-	
-	// btnVstPathShow
-	const btnVstPathShow = Content.getComponent("btnVstPathShow");
-	btnVstPathShow.setControlCallback(onbtnVstPathShowControl);
-	
-	inline function onbtnVstPathShowControl(component, value)
-	{
-		if (value)
-			openPath(lblVstPath.get("text"));
-	}
-		
-	// btnAuPath
-	const btnAuPath = Content.getComponent("btnAuPath");
-	btnAuPath.set("enabled", Engine.getOS() == "OSX");
-	btnAuPath.setControlCallback(onbtnAuPathControl);
-	
-	inline function onbtnAuPathControl(component, value)
-	{
-		if (value)
-			setAuPath();
-	}
-		
-	// lblAuPath
-	const lblAuPath = Content.getComponent("lblAuPath");
-	lblAuPath.set("text", "");	
-		
-	// btnAuPathShow
-	const btnAuPathShow = Content.getComponent("btnAuPathShow");
-	btnAuPathShow.set("enabled", Engine.getOS() == "OSX");
-	btnAuPathShow.setControlCallback(onbtnAuPathShowControl);
 
-	inline function onbtnAuPathShowControl(component, value)
-	{
-		if (value)
-			openPath(lblAuPath.get("text"));
-	}
+		g.drawAlignedText("Default Sample Path", [40, lblSamplePath.get("y"), 200, 30], "left");
+		g.drawAlignedText("VST3 Path", [40, lblVst3Path.get("y"), 200, 30], "left");
+
+		if (lblAuPath.get("visible"))
+			g.drawAlignedText("AU Path", [40, lblAuPath.get("y"), 200, 30], "left");
+
+		g.drawAlignedText("Auto Sync", [40, btnAutoSync.get("y"), 200, btnAutoSync.getHeight()], "left");
+		g.drawAlignedText(["Off", "On"][btnAutoSync.getValue()], [btnAutoSync.get("x") + btnAutoSync.getWidth() + 15, btnAutoSync.get("y"), 200, btnAutoSync.getHeight()], "left");
 		
+		g.drawAlignedText("Beta Updates", [40, btnBeta.get("y"), 200, btnBeta.getHeight()], "left");
+		g.drawAlignedText(["Off", "On"][btnBeta.getValue()], [btnBeta.get("x") + btnBeta.getWidth() + 15, btnBeta.get("y"), 200, btnBeta.getHeight()], "left");
+	});
+
 	// btnAutoSync
 	const btnAutoSync = Content.getComponent("btnAutoSync");
+	btnAutoSync.setLocalLookAndFeel(LookAndFeel.toggleButton);
 	btnAutoSync.setControlCallback(onbtnAutoSyncControl);
 
 	inline function onbtnAutoSyncControl(component, value)
 	{
-		set("autoSync", value);
 		pnlSettingsTab[0].repaint();
 	}
 	
-	// btnManualInstall
-	const btnManualInstall = Content.getComponent("btnManualInstall");
-	btnManualInstall.showControl(!Engine.isPlugin());
-	btnManualInstall.setControlCallback(onbtnManualInstallControl);
+	// btnBeta
+	const btnBeta = Content.getComponent("btnBeta");
+	btnBeta.setLocalLookAndFeel(LookAndFeel.toggleButton);
+	btnBeta.setControlCallback(onbtnBetaControl);
 	
-	inline function onbtnManualInstallControl(component, value)
+	inline function onbtnBetaControl(component, value)
 	{
-	    if (value)
-	    {
-		    pnlSettings.showControl(false);
-		    Expansions.manualInstall();
-	    }
+		pnlSettingsTab[0].repaint();
 	}
+
+	// lblSamplePath
+	const lblSamplePath = Content.getComponent("lblSamplePath");
+	lblSamplePath.set("text", "");
+
+	// lblVst3Path
+	const lblVst3Path = Content.getComponent("lblVst3Path");
 	
-	// btnClearTemp
-	const btnClearTemp = Content.getComponent("btnClearTemp");
-	btnClearTemp.showControl(!Engine.isPlugin());
-	btnClearTemp.setControlCallback(onbtnClearTempControl);
+	// lblAuPath
+	const lblAuPath = Content.getComponent("lblAuPath");
+	lblAuPath.showControl(Engine.getOS() == "OSX");
 	
-	inline function onbtnClearTempControl(component, value)
+	// btnSamplePath
+	const btnSamplePath = Content.getComponent("btnSamplePath");
+	btnSamplePath.setLocalLookAndFeel(LookAndFeel.iconButton);
+	btnSamplePath.setControlCallback(onbtnSamplePathControl);
+	
+	inline function onbtnSamplePathControl(component, value)
 	{
-		if (value)
+		if (!value)
 		{
-			Engine.showYesNoWindow("Delete Files", "Do you want to delete temporary files?", function(response)
+			local startFolder = FileSystem.Desktop;	
+
+			FileSystem.browseForDirectory(startFolder, function(dir)
 			{
-				if (response)
-					DownloadManager.clearTempDirectory();
+				if (isDefined(dir) && dir.isDirectory())
+				{
+					lblSamplePath.set("text", dir.toString(dir.FullPath));
+				}
 			});
 		}
 	}
-	
-	// btnBitDepth
-	const btnBitDepth = Content.getComponent("btnBitDepth");
-	btnBitDepth.setControlCallback(onbtnBitDepthControl);
-	
-	inline function onbtnBitDepthControl(component, value)
+		
+	// pnlSettingsTab1 - Engine
+	pnlSettingsTab[1].setPaintRoutine(function(g)
 	{
-		set("installFullDynamics", value);
-		pnlSettingsTab[0].repaint();
+		var a = this.getLocalBounds(0);
+
+		// Labels
+		g.setColour(this.get("textColour"));
+		g.setFont("medium", 18);
+
+		g.drawAlignedText("UI Scaling", [40, cmbZoom.get("y"), 200, 30], "left");
+		g.drawAlignedText("Open GL", [40, cmbOpenGl.get("y"), 200, 30], "left");
+		g.drawAlignedText("Disk Streaming", [40, cmbStreaming.get("y"), 200, 30], "left");
+
+		if (cmbTheme.get("visible"))
+			g.drawAlignedText("UI Theme", [40, cmbTheme.get("y"), 200, 30], "left");
+	});
+	
+	// cmbZoom
+	const cmbZoom = Content.getComponent("cmbZoom");
+	cmbZoom.setLocalLookAndFeel(LookAndFeel.comboBox);
+	cmbZoom.setControlCallback(oncmbZoomControl);
+	
+	inline function oncmbZoomControl(component, value)
+	{
+		local zl = parseFloat(component.getItemText()) / 100;
+		Settings.setZoomLevel(zl);
+	}
+
+	// cmbOpenGl
+	const cmbOpenGl = Content.getComponent("cmbOpenGl");
+	cmbOpenGl.setLocalLookAndFeel(LookAndFeel.comboBox);
+	cmbOpenGl.setControlCallback(oncmbOpenGlControl);
+	
+	inline function oncmbOpenGlControl(component, value)
+	{
+		Settings.setEnableOpenGL(value == 1);
 	}
 	
-	// btnSettingsClose
-	const btnSettingsClose = Content.getComponent("btnSettingsClose");
-	btnSettingsClose.setControlCallback(onbtnSettingsCloseControl);
-	
-	inline function onbtnSettingsCloseControl(component, value)
+	// cmbStreaming
+	const cmbStreaming = Content.getComponent("cmbStreaming");
+	cmbStreaming.setLocalLookAndFeel(LookAndFeel.comboBox);
+	cmbStreaming.setControlCallback(oncmbStreamingControl);
+
+	inline function oncmbStreamingControl(component, value)
 	{
-		if (value)
-			pnlSettings.showControl(false);
+		Settings.setDiskMode(value == 1);
 	}
 	
+	// cmbTheme
+	const cmbTheme = Content.getComponent("cmbTheme");
+	cmbTheme.setLocalLookAndFeel(LookAndFeel.comboBox);
+	cmbTheme.setControlCallback(oncmbThemeControl);
+	cmbTheme.set("items", Theme.getIds().join("\n"));
+	
+	inline function oncmbThemeControl(component, value)
+	{
+		Theme.setStyle(value - 1);
+	}
+
 	// Functions
-	inline function openPath(path)
+	inline function changeTab(index)
 	{
-		if (path != "")
+		for (i = 0; i < NUM_TABS; i++)
+			pnlSettingsTab[i].showControl(i == index);
+	}
+	
+	inline function setPathLabelText()
+	{
+		switch (Engine.getOS())
 		{
-			local f = FileSystem.fromAbsolutePath(path);
-			
-			if (f.isDirectory())
-				f.show();
+			case "LINUX":
+				lblVst3Path.set("text", "~/.vst3");
+				lblAuPath.set("text", "");
+				break;
+
+			case "OSX":
+				lblVst3Path.set("text", "Library/Audio/Plug-ins/VST3");
+				lblAuPath.set("text", "Library/Audio/Plug-ins/Components");
+				break;
+
+			case "WIN":
+				lblVst3Path.set("text", "C:\Program Files\Common Files\VST3");
+				lblAuPath.set("text", "");
+				break;
 		}
 	}
 	
-	inline function getVstPath()
+	inline function saveAndExit()
 	{
-		if (isDefined(data.vstPath))
-			return data.vstPath
+		local f = appData.getChildFile("userSettings.json");
+		
+		if (isDefined(f) && f.isFile())
+			data = f.loadAsObject();
 			
-		return false;
+		data.autoSync = btnAutoSync.getValue();
+		data.beta = btnBeta.getValue();
+		data.samplePath = lblSamplePath.get("text");
+		data.zoom = cmbZoom.getValue();
+		data.openGl = cmbOpenGl.getValue();
+		data.streaming = cmbStreaming.getValue();
+		data.theme = cmbTheme.getValue();
+		
+		f.writeObject(data);
+
+		hide();
 	}
 	
-	inline function getAuPath()
+	inline function restoreSettings()
 	{
-		if (isDefined(data.auPath))
-			return data.auPath
-			
-		return false;
-	}
-	
-	inline function setVstPath()
-	{
-		FileSystem.browseForDirectory(FileSystem.UserHome, function(dir)
+		local f = appData.getChildFile("userSettings.json");
+
+		if (isDefined(f) && f.isFile())
 		{
-			lblVstPath.set("text", dir.toString(File.FullPath));
-			data.vstPath = lblVstPath.get("text");
-			write();
-		});
-	}
-	
-	inline function setAuPath()
-	{
-		FileSystem.browseForDirectory(FileSystem.UserHome, function(dir)
-		{
-			lblAuPath.set("text", dir.toString(File.FullPath));
-			data.auPath = lblAuPath.get("text");
-			write();				
-		});
-	}
-	
-	inline function setDefaultPluginPaths()
-	{
-		if (!arePathsSet())
-		{
-			local vst = "";
-			local au = "";
-	
-			switch (Engine.getOS())
+			data = f.loadAsObject();
+
+			if (isDefined(data.autoSync)) btnAutoSync.setValue(data.autoSync);
+			if (isDefined(data.beta)) btnBeta.setValue(data.beta);
+			if (isDefined(data.samplePath)) lblSamplePath.set("text", data.samplePath);
+			if (isDefined(data.zoom)) cmbZoom.setValue(data.zoom);
+			if (isDefined(data.openGl)) cmbOpenGl.setValue(data.openGl);
+			if (isDefined(data.streaming)) cmbStreaming.setValue(data.streaming);
+
+			if (isDefined(data.theme))
 			{
-				case "OSX":
-					vst = "Library/Audio/Plug-ins/VST3";
-					au = "/Library/Audio/Plug-Ins/Components";
-				break;
-				
-				case "LINUX":
-					vst = "~/.vst3";
-				break;
-				
-				case "WIN":
-					vst = "C:\Program Files\Common Files\VST3";
-				break;
+				cmbTheme.setValue(data.theme);
+				Theme.setStyle(data.theme - 1);
 			}
-			
-			lblVstPath.set("text", vst);
-			lblAuPath.set("text", au);
-			
-			data.vstPath = vst;
-			data.auPath = au;
-	
-			write();
 		}
 	}
 	
-	inline function arePathsSet()
-	{		
-		if (Engine.getOS() == "OSX")
-			return data.vstPath != "" && data.auPath != "";
+	inline function setValue(key, value)
+	{
+		local f = appData.getChildFile("userSettings.json");
+		
+		if (isDefined(f) && f.isFile())
+			data = f.loadAsObject();
 			
-		return data.vstPath != "";		
+		data[key] = value;
+
+		return f.writeObject(data);		
 	}
 	
-	inline function set(field, value)
+	inline function getValue(key)
 	{
-		data[field] = value;
-		write();
+		if (!isDefined(data[key]))
+			return undefined;
+
+		return data[key];
 	}
 	
-	inline function get(field)
+	inline function show()
 	{
-		if (isDefined(data[field]))
-			return data[field];
+		pnlSettings.showControl(true);
+	}
+	
+	inline function hide()
+	{
+		pnlSettings.showControl(false);
+	}
+	
+	inline function getDefaultSampleFolder()
+	{
+		local path = data.samplePath;
+		local f;
+	
+		if (isDefined(path))
+			f = FileSystem.fromAbsolutePath(path);
+	
+		if (isDefined(f) && f.isDirectory())
+			return f;
 
 		return undefined;
 	}
 	
-	inline function write()
-	{
-		local f = appData.getChildFile("settings.json");
-		f.writeObject(data);
-	}
-	
-	inline function read()
-	{
-		local f = appData.getChildFile("settings.json");
-		
-		if (f.isFile())
-		{
-			local d = f.loadAsObject();
-			
-			if (d != undefined)
-			{
-				if (isDefined(d.vstPath))
-					lblVstPath.set("text", d.vstPath);
-				
-				if (isDefined(d.auPath))
-					lblAuPath.set("text", d.auPath);
-					
-				if (isDefined(d.autoSync))
-					btnAutoSync.setValue(d.autoSync);
-			}
-			
-			data = d;
-		}
-	}
-	
-	// Init calls
-	read();
-	setDefaultPluginPaths();
+	// Function calls
+	restoreSettings();
+	setPathLabelText();
 }
